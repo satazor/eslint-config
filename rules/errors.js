a37a7df434b298fe9d9c99ac8a490277c2ab0507
André Cruz <andremiguelcruz@msn.com>
2015-12-29 19:50:05 +0000

    Update rules.

--- rules/errors.js
+++ rules/errors.js
@@ -35,7 +35,7 @@ module.exports = {
         // disallow double-negation boolean casts in a boolean context
         'no-extra-boolean-cast': 2,
         // disallow unnecessary parentheses
-        'no-extra-parens': 2,
+        'no-extra-parens': [2, 'functions'],
         // disallow unnecessary semicolons
         'no-extra-semi': 2,
         // disallow overwriting functions written as function declarations

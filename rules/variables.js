384cf7ab9bbe82dd265c00c199ac6dbea30c637d
André Cruz <andremiguelcruz@msn.com>
2015-12-29 18:28:06 +0000

    Update rules.

--- rules/variables.js
+++ rules/variables.js
@@ -15,7 +15,7 @@ module.exports = {
         // disallow shadowing of names such as arguments
         'no-shadow-restricted-names': 2,
         // disallow declaration of variables already declared in the outer scope
-        'no-shadow': 2,
+        'no-shadow': 0,
         // disallow use of undefined when initializing variables
         'no-undef-init': 2,
         // disallow use of undeclared variables unless mentioned in a /*global */ block
@@ -25,6 +25,6 @@ module.exports = {
         // disallow declaration of variables that are not used in the code
         'no-unused-vars': [2, { 'vars': 'local', 'args': 'after-used' }],
         // disallow use of variables before they are defined
-        'no-use-before-define': 2,
+        'no-use-before-define': 0,
     },
 };
